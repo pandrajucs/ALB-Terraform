884f51a74a5f104119fc28db941794591253b657
C S <pandrajucs@example.com>
2020-09-19 14:34:51 +0530

    First commit

---
+++ main.tf
@@ -0,0 +1,190 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+    
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "vpc-03fce764d6d3fdd1f"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "server-1" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-034250c45e4a08fbf"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script1.sh")}"
+    tags = {
+        Name = "Home-Server-1"
+    }
+} 
+
+
+
+resource "aws_instance" "server-2" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1a"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-034250c45e4a08fbf"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script2.sh")}"
+    tags = {
+       Name = "Home-Server-2"
+   }
+} 
+
+resource "aws_instance" "server-3" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1a"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-034250c45e4a08fbf"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script3.sh")}"
+    tags = {
+       Name = "Home-Server-3"
+   }
+} 
+
+resource "aws_instance" "server-4" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1b"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-0d47b4ff30b766a68"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script4.sh")}"
+    tags = {
+        Name = "Movies-Server-1"
+    }
+} 
+
+
+
+resource "aws_instance" "server-5" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1b"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-0d47b4ff30b766a68"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script5.sh")}"
+    tags = {
+       Name = "Movies-Server-2"
+   }
+} 
+
+resource "aws_instance" "server-6" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1b"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-0d47b4ff30b766a68"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script6.sh")}"
+    tags = {
+       Name = "Movies-Server-3"
+   }
+} 
+
+resource "aws_instance" "server-7" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1c"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-0e603ab3ebf20d410"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script7.sh")}"
+    tags = {
+        Name = "Shows-Server-1"
+    }
+} 
+
+
+
+resource "aws_instance" "server-8" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1c"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-0e603ab3ebf20d410"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script8.sh")}"
+    tags = {
+       Name = "Shows-Server-2"
+   }
+} 
+
+resource "aws_instance" "server-9" {
+   ami = "ami-00514a528eadbc95b"
+   availability_zone = "us-east-1c"
+   instance_type = "t2.micro"
+   key_name = "AWS_KeyPair"
+   subnet_id = "subnet-0e603ab3ebf20d410"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script9.sh")}"
+    tags = {
+       Name = "Shows-Server-3"
+   }
+} 
+
+resource "aws_lb_target_group" "htg" {
+  name     = "Home-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-03fce764d6d3fdd1f"
+}
+
+resource "aws_lb_target_group" "mtg" {
+  name     = "Movies-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-03fce764d6d3fdd1f"
+}
+
+resource "aws_lb_target_group" "stg" {
+  name     = "Shows-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-03fce764d6d3fdd1f"
+}
+
+resource "aws_lb" "alb" {
+  name               = "aws-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-034250c45e4a08fbf","subnet-0d47b4ff30b766a68","subnet-0e603ab3ebf20d410"]
+
+} 
+
+
+
---
+++ targetgroup.tf
@@ -0,0 +1,103 @@
+resource "aws_lb_target_group_attachment" "tg1" {
+  target_group_arn = "${aws_lb_target_group.htg.arn}"
+  target_id        = "${aws_instance.server-1.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg2" {
+  target_group_arn = "${aws_lb_target_group.htg.arn}"
+  target_id        = "${aws_instance.server-2.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg3" {
+  target_group_arn = "${aws_lb_target_group.htg.arn}"
+  target_id        = "${aws_instance.server-3.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg4" {
+  target_group_arn = "${aws_lb_target_group.mtg.arn}"
+  target_id        = "${aws_instance.server-4.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg5" {
+  target_group_arn = "${aws_lb_target_group.mtg.arn}"
+  target_id        = "${aws_instance.server-5.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg6" {
+  target_group_arn = "${aws_lb_target_group.mtg.arn}"
+  target_id        = "${aws_instance.server-6.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg7" {
+  target_group_arn = "${aws_lb_target_group.stg.arn}"
+  target_id        = "${aws_instance.server-7.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg8" {
+  target_group_arn = "${aws_lb_target_group.stg.arn}"
+  target_id        = "${aws_instance.server-8.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg9" {
+  target_group_arn = "${aws_lb_target_group.stg.arn}"
+  target_id        = "${aws_instance.server-9.id}"
+  port             = 80
+}
+
+
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = "${aws_lb.alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.htg.arn}"
+  }
+}
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn ="${aws_lb_listener.web.arn}" 
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.mtg.arn}"
+  }
+
+  condition {
+    path_pattern {
+      values = ["/movies/"]
+    }
+   }
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.stg.arn}"
+  }
+
+  condition {
+    path_pattern {
+      values = ["/shows/"]
+    }
+   }
+
+  }
+
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z0880630DSDGCDTP0JHD"
+  name    = "www.pandrajucs.in"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.alb.dns_name}"
+    zone_id                = "${aws_lb.alb.zone_id}"
+    evaluate_target_health = false
+  }
+}
